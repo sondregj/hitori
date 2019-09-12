2ad9eafa587db24563e5ae2544bd1f61e62a19e4
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-09-12 04:35:53 +0200

    :sparkles: HitoriBoard class

---
+++ src/core/errors.ts
@@ -0,0 +1,1 @@
+export class InvalidArrayError extends Error {}
---
+++ src/core/index.ts
@@ -0,0 +1,6 @@
+import HitoriBoard from './board'
+
+export { IHitoriBoard, IHitoriRow, IHitoriColumn, IHitoriCell } from './types'
+export { solve } from './solver'
+
+export default HitoriBoard
