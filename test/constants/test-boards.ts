7ba761caa8ab841e0f8116e0206be5ada7ee2725
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-09-17 03:52:47 +0200

    :white_check_mark: Added test boards

---
+++ test/constants/test-boards.ts
@@ -0,0 +1,27 @@
+export const testBoard: number[][] = [
+    [5, 2, 1, 6, 2, 5],
+    [3, 1, 4, 2, 6, 6],
+    [4, 2, 3, 4, 6, 3],
+    [4, 5, 6, 3, 2, 2],
+    [2, 4, 3, 3, 4, 5],
+    [6, 4, 6, 5, 3, 3],
+]
+
+export const invalid: number[][] = [[1, 2, 3], [1, 2], [1, 2, 3, 4]]
+
+export const fiveByFive: number[][] = []
+
+export const unsolvable: number[][] = [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+]
+
+export default {
+    fiveByFive,
+    invalid,
+    testBoard,
+    unsolvable,
+}
